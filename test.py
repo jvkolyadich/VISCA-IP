07904e18e4061f37734747838bd80eff66870913
James K <jvkolyadich@gmail.com>
2020-11-17 21:59:56 -0500

    cleaned up controller methods

---
+++ test.py
@@ -0,0 +1,4 @@
+import viscaip.controller as vip
+
+cam = vip.Controller("192.168.1.129")
+print(cam.powerOn())
--- viscaip/controller.py
+++ viscaip/controller.py
@@ -9,8 +9,6 @@ class Controller:
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         # Timeout when receiving data
         self.connection.settimeout(2.0)
-        # Buffer size for receiving data
-        self.recv_buffsize = 1024
         # The camera needs every command sent to it to be numbered
         self.command_number = 0
 
@@ -18,7 +16,7 @@ class Controller:
         self.command_number += 1
         return self.command_number
 
-    def _send_to_cam(self, hex_command):
+    def _sendToCam(self, hex_command):
         command_type = bytearray.fromhex('01 00')
         command_body = bytearray.fromhex(hex_command)
         command_length = len(command_body).to_bytes(2, 'big')
@@ -26,126 +24,127 @@ class Controller:
         command = command_type + command_length + command_number + command_body
         self.connection.sendto(command, (self.ip, self.port))
         try:
-            response = self.connection.recvfrom(self.recv_buffsize)
+            # 1024 is the buffer size for receiving data
+            response = self.connection.recvfrom(1024)
         except socket.timeout:
             raise TimeoutError("Camera took too long to respond")
-        return binascii.hexlify(response[0])
+        return binascii.hexlify(response[0]).hex()
+
+    def _intToHex(self, number):
+        # The '[2:]' removes '0x' from the beginning of the hex string
+        return hex(number)[2:]
+
+    def _zoomPositionValid(self, position):
+        if (0 <= position <= 16384):
+            return True
+        else:
+            raise ZoomPositionError
+
+    def _zoomSpeedValid(self, speed):
+        if (0 <= speed <= 7):
+            return True
+        else:
+            raise ZoomSpeedError
+
+    def _moveSpeedValid(self, speed):
+        if (1 <= speed <= 17):
+            return True
+        else:
+            raise MoveSpeedError
 
     def powerOn(self):
         hex_command = "81 01 04 00 02 FF"
-        return self._send_to_cam(hex_command)
+        return self._sendToCam(hex_command)
 
     def powerOff(self):
         hex_command = "81 01 04 00 03 FF"
-        return self._send_to_cam(hex_command)
+        return self._sendToCam(hex_command)
 
     def zoomStop(self):
         hex_command = "81 01 04 07 00 FF"
-        return self._send_to_cam(hex_command)
+        return self._sendToCam(hex_command)
 
     def zoomIn(self, speed = None):
         if speed is None:
             hex_command = "81 01 04 07 02 FF"
-            return self._send_to_cam(hex_command)
+            return self._sendToCam(hex_command)
         else:
-            if (0 <= speed <= 7):
+            if (self._zoomSpeedValid(speed)):
                 hex_command = f"81 01 04 07 2{speed} FF"
-                return self._send_to_cam(hex_command)
-            else:
-                raise ZoomSpeedError
+                return self._sendToCam(hex_command)
 
     def zoomOut(self, speed = None):
         if speed is None:
             hex_command = "81 01 04 07 03 FF"
-            return self._send_to_cam(hex_command)
+            return self._sendToCam(hex_command)
         else:
-            if (0 <= speed <= 7):
+            if (self._zoomSpeedValid(speed)):
                 hex_command = f"81 01 04 07 3{speed} FF"
-                return self._send_to_cam(hex_command)
-            else:
-                raise ZoomSpeedError
+                return self._sendToCam(hex_command)
     
     def zoomSet(self, position):
-        if (0 <= position <= 16384):
-            # The '[2:]' removes '0x' from the beginning of the hex string
-            hex_position = hex(position)[2:]
+        if (self._zoomPositionValid(position)):
+            hex_position = self._intToHex(position)
+            # Splits a 4-character hex number into 4 different variables
             s = hex_position[len(hex_position) - 1]
             r = hex_position[len(hex_position) - 2] or "0"
             q = hex_position[len(hex_position) - 3] or "0"
             p = hex_position[len(hex_position) - 4] or "0"
             hex_command = f"81 01 04 47 0{p} 0{q} 0{r} 0{s} FF"
-            return self._send_to_cam(hex_command)
-        else:
-            raise ZoomPositionError
+            return self._sendToCam(hex_command)
     
     def moveStop(self):
         hex_command = "81 01 06 01 00 00 03 03 FF"
-        return self._send_to_cam(hex_command)
+        return self._sendToCam(hex_command)
     
     def moveUp(self, speed):
-        if (1 <= speed <= 17):
-            hex_speed = hex(speed)[2:]
+        if (self._moveSpeedValid(speed)):
+            hex_speed = self._intToHex(speed)
             hex_command = f"81 01 06 01 {hex_speed} {hex_speed} 03 01 FF"
-            return self._send_to_cam(hex_command)
-        else:
-            raise MoveSpeedError
+            return self._sendToCam(hex_command)
 
     def moveDown(self, speed):
-        if (1 <= speed <= 17):
-            hex_speed = hex(speed)[2:]
+        if (self._moveSpeedValid(speed)):
+            hex_speed = self._intToHex(speed)
             hex_command = f"81 01 06 01 {hex_speed} {hex_speed} 03 02 FF"
-            return self._send_to_cam(hex_command)
-        else:
-            raise MoveSpeedError
+            return self._sendToCam(hex_command)
 
     def moveLeft(self, speed):
-        if (1 <= speed <= 17):
-            hex_speed = hex(speed)[2:]
+        if (self._moveSpeedValid(speed)):
+            hex_speed = self._intToHex(speed)
             hex_command = f"81 01 06 01 {hex_speed} {hex_speed} 01 03 FF"
-            return self._send_to_cam(hex_command)
-        else:
-            raise MoveSpeedError
+            return self._sendToCam(hex_command)
 
     def moveRight(self, speed):
-        if (1 <= speed <= 17):
-            hex_speed = hex(speed)[2:]
+        if (self._moveSpeedValid(speed)):
+            hex_speed = self._intToHex(speed)
             hex_command = f"81 01 06 01 {hex_speed} {hex_speed} 02 03 FF"
-            return self._send_to_cam(hex_command)
-        else:
-            raise MoveSpeedError
+            return self._sendToCam(hex_command)
 
     def moveUpLeft(self, speed):
-        if (1 <= speed <= 17):
-            hex_speed = hex(speed)[2:]
+        if (self._moveSpeedValid(speed)):
+            hex_speed = self._intToHex(speed)
             hex_command = f"81 01 06 01 {hex_speed} {hex_speed} 01 01 FF"
-            return self._send_to_cam(hex_command)
-        else:
-            raise MoveSpeedError
+            return self._sendToCam(hex_command)
 
     def moveUpRight(self, speed):
-        if (1 <= speed <= 17):
-            hex_speed = hex(speed)[2:]
+        if (self._moveSpeedValid(speed)):
+            hex_speed = self._intToHex(speed)
             hex_command = f"81 01 06 01 {hex_speed} {hex_speed} 02 01 FF"
-            return self._send_to_cam(hex_command)
-        else:
-            raise MoveSpeedError
+            return self._sendToCam(hex_command)
 
     def moveDownLeft(self, speed):
-        if (1 <= speed <= 17):
-            hex_speed = hex(speed)[2:]
+        if (self._moveSpeedValid(speed)):
+            hex_speed = self._intToHex(speed)
             hex_command = f"81 01 06 01 {hex_speed} {hex_speed} 02 01 FF"
-            return self._send_to_cam(hex_command)
-        else:
-            raise MoveSpeedError
+            return self._sendToCam(hex_command)
 
     def moveDownRight(self, speed):
-        if (1 <= speed <= 17):
-            hex_speed = hex(speed)[2:]
+        if (self._moveSpeedValid(speed)):
+            hex_speed = self._intToHex(speed)
             hex_command = f"81 01 06 01 {hex_speed} {hex_speed} 02 02 FF"
-            return self._send_to_cam(hex_command)
-        else:
-            raise MoveSpeedError
+            return self._sendToCam(hex_command)
     
     def moveToCenter(self):
         hex_command = "81 01 06 04 FF"
-        return self._send_to_cam(hex_command)
+        return self._sendToCam(hex_command)
