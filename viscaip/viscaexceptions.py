4dec887fffef803a6770471880fd7183ee538d6f
James K <65877175+jvkolyadich@users.noreply.github.com>
2020-12-13 16:23:26 -0500

    added presets exceptions to viscaexceptions.py

--- viscaip/viscaexceptions.py
+++ viscaip/viscaexceptions.py
@@ -13,12 +13,17 @@ class MoveSpeedError(BaseException):
         self.message = "Move speed must be between 1 and 17"
         super().__init__(self.message)
 
-class PanPosError(BaseException):
+class PanPositionError(BaseException):
     def __init__(self):
         self.message = "Pan position must be between -1660 and 1660"
         super().__init__(self.message)
 
-class TiltPosError(BaseException):
+class TiltPositionError(BaseException):
     def __init__(self):
         self.message = "Tilt position must be between -374 and 1122"
+        super().__init__(self.message)
+
+class PresetError(BaseException):
+    def __init__(self):
+        self.message = "Invalid preset. Only presets 1 through 9 are supported"
         super().__init__(self.message)
